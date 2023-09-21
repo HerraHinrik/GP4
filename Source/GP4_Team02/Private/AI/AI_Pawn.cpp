6e6dd07f51180565c957a7deea6fba0117901805
Ekrick <113420224+Ekrick@users.noreply.github.com>
2023-09-21 11:40:23 +0200

    ai pawn
    removed unnecessary event subscribe

--- Source/GP4_Team02/Private/AI/AI_Pawn.cpp
+++ Source/GP4_Team02/Private/AI/AI_Pawn.cpp
@@ -11,9 +11,7 @@ void AAI_Pawn::BeginPlay()
 
 	myTeam = GameManager->AssignAITeam();
 	GameManager->AddPawnToArray(this);
-
-	GameManager->OnTurnChanged.AddDynamic(this, &AAI_Pawn::CheckIfMyTurn);
-
+	
 }
 
 void AAI_Pawn::Tick(float DeltaSeconds)
