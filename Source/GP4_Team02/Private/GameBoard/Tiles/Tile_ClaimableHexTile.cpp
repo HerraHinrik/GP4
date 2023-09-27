f4fa7da0f659255634f3a19f625cb636e0b32e6c
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-09-27 12:16:59 +0200

    Tile material changes when claimed

--- Source/GP4_Team02/Private/GameBoard/Tiles/Tile_ClaimableHexTile.cpp
+++ Source/GP4_Team02/Private/GameBoard/Tiles/Tile_ClaimableHexTile.cpp
@@ -15,5 +15,13 @@ void UTile_ClaimableHexTile::ClaimTile(TObjectPtr<ATeam> NewClaimingTeam)
 	}
 	ClaimingTeam = NewClaimingTeam;
 	ClaimingTeam->AddClaimedTile(this);
+
+	// Set the material of the tile to the material of the claiming team
+	if(ClaimingTeam->ClaimedTileMaterial)
+	{
+		UMaterialInstanceDynamic* DynamicMaterial = UMaterialInstanceDynamic::Create(ClaimingTeam->ClaimedTileMaterial, this);
+		SetMaterial(0, DynamicMaterial);
+	}
+	
 	
 }
