fe072396870a8a660941fbeb81cfee4350b162a1
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-09-27 14:36:06 +0200

    Added event on claim tile

--- Source/GP4_Team02/Public/GameBoard/Tiles/Tile_ClaimableHexTile.h
+++ Source/GP4_Team02/Public/GameBoard/Tiles/Tile_ClaimableHexTile.h
@@ -6,6 +6,8 @@
 #include "HexTile.h"
 #include "Tile_ClaimableHexTile.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnClaimTile);
+
 class ATeam;
 /**
  * 
@@ -17,6 +19,9 @@ class GP4_TEAM02_API UTile_ClaimableHexTile : public UHexTile
 public:
 	virtual void ClaimTile(TObjectPtr<ATeam> NewClaimingTeam);
 	TObjectPtr<ATeam> GetClaimingTeam() const { return ClaimingTeam; }
+
+	UPROPERTY( BlueprintAssignable )
+	FOnClaimTile OnClaimTileEvent;
 	
 protected:
 	TObjectPtr<ATeam> ClaimingTeam;
